0321d620920425b3f9cf7da95b6ea9ec14ed06d1
Denys Kukushkin <denys.kukushkin@gmail.com>
2015-05-21 20:19:48 -0400

    Settings file added

---
+++ settings.py
@@ -0,0 +1,23 @@
+# This is a file with all settings and options that we have for the AnnotateCilitate program
+
+import multiprocessing
+
+Options = {
+'Telomeres' : True, 
+'MDS' : True,
+'Pointers' : True, 
+'TelomereEndLimit' : 100,
+'TelomereOffset' : 5,
+'BlastMaskLowercase' : True,
+'RoughBlastTask' : 'megablast',
+'RoughBlastWordSize' : 28,
+'RoughBlastDust' : True,
+'RoughBlastUngapped' : True,
+'RoughCoverageLimit' : 5,
+'FineBlastTask' : 'blastn-short',
+'FineBlastWordSize' : 15,
+'FineBlastDust' : False,
+'FineBlastUngapped' : True,
+'ThreadCount' : 2*multiprocessing.cpu_count(),
+'DatabaseUpdate' : True
+}
